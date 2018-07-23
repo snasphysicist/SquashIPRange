1571f370c374191dea20dbb09a486da92621b3d2
snasphysicist <ih8coldplay@hotmail.com>
2018-07-23 19:01:53 +0100

    Updated setUpConstraints to allow a fill argument, and updated buttons to fill their columns so they are of equal width in each column.

--- src/squashiprange/SquashIPRangeUINew.java
+++ src/squashiprange/SquashIPRangeUINew.java
@@ -14,10 +14,11 @@ package squashiprange;
  */
 public class SquashIPRangeUINew {
     
-    private static java.awt.GridBagConstraints setUpConstraints( int column , int row ) {
+    private static java.awt.GridBagConstraints setUpConstraints( int column , int row , int fill ) {
         java.awt.GridBagConstraints constraints = new java.awt.GridBagConstraints() ;
         constraints.gridx = column ;
         constraints.gridy = row ;
+        constraints.fill = fill ;
         return constraints ;
     }
     
@@ -40,31 +41,38 @@ public class SquashIPRangeUINew {
         
         //Reformat
         javax.swing.JButton reformatButton = new javax.swing.JButton( "Reformat" ) ;
-        mainFrame.add( reformatButton , setUpConstraints( 1 , 1 ) ) ;
+        mainFrame.add( reformatButton , setUpConstraints( 1, 1, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //Squash
         javax.swing.JButton squashButton = new javax.swing.JButton( "Squash" ) ;
-        mainFrame.add( squashButton , setUpConstraints( 1 , 2 ) ) ;
+        mainFrame.add( squashButton , setUpConstraints( 1, 2, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //Find Overlap
         javax.swing.JButton overlapButton = new javax.swing.JButton( "Find Overlap" ) ;
-        mainFrame.add( overlapButton , setUpConstraints( 1 , 3 ) ) ;
+        mainFrame.add( overlapButton , setUpConstraints( 1, 3, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //Clear Input
         javax.swing.JButton clearInputButton = new javax.swing.JButton( "Clear Input" ) ;
-        mainFrame.add( clearInputButton , setUpConstraints( 1 , 4 ) ) ;
+        mainFrame.add( clearInputButton , setUpConstraints( 1, 4, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //To Clipboard
         javax.swing.JButton clipboardButton = new javax.swing.JButton( "To Clipboard" ) ;
-        mainFrame.add( clipboardButton , setUpConstraints( 2 , 1 ) ) ;
+        mainFrame.add( clipboardButton , setUpConstraints( 2, 1, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //Close
         javax.swing.JButton closeButton = new javax.swing.JButton( "Close" ) ;
-        mainFrame.add( closeButton , setUpConstraints( 2 , 4 ) ) ;
+        mainFrame.add( closeButton , setUpConstraints( 2, 4, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //Help
         javax.swing.JButton aboutButton = new javax.swing.JButton( "About" ) ;
-        mainFrame.add( aboutButton , setUpConstraints( 2 , 5 ) ) ;
+        mainFrame.add( aboutButton , setUpConstraints( 2, 5, 
+                                                          java.awt.GridBagConstraints.BOTH ) ) ;
         
         //Exit on close
         mainFrame.setDefaultCloseOperation( javax.swing.JFrame.EXIT_ON_CLOSE ) ;
