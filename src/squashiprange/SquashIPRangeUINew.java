00c3bd1d60443f871455f44904afa66997c2c1e7
snasphysicist <ih8coldplay@hotmail.com>
2018-07-27 16:11:59 +0100

    Added JPanels to subdivide the JFrame, and put the buttons/etc inside the JPanels.

--- src/squashiprange/SquashIPRangeUINew.java
+++ src/squashiprange/SquashIPRangeUINew.java
@@ -14,20 +14,44 @@ package squashiprange;
  */
 public class SquashIPRangeUINew {
     
+    //Grid Bag Constrains constants
     private static final int GBBOTH = java.awt.GridBagConstraints.BOTH ;
     private static final int GBNONE = java.awt.GridBagConstraints.BOTH ;
+    private static final int GBCENTER = java.awt.GridBagConstraints.CENTER ;
+    private static final int GBRIGHT = java.awt.GridBagConstraints.EAST ;
+    //textArea sizes (in arbitrary units)
+    private static final int TEXTAREAWIDTH = 20 ;
+    private static final int TEXTAREAHEIGHT = 30 ;
+    
+    //Generate titled border objects
+    private static javax.swing.border.Border titledBorder( String title ) {
+        javax.swing.border.Border theBorder = javax.swing.BorderFactory.createEtchedBorder() ;
+        return javax.swing.BorderFactory.createTitledBorder( theBorder , title ) ;
+    }
     
     //Most general setUpConstraints method
     private static java.awt.GridBagConstraints setUpConstraints( int column , int row , 
                                                                  int fill , int columns ,
-                                                                 int rows ) {
+                                                                 int rows , int anchor ) {
         java.awt.GridBagConstraints constraints = new java.awt.GridBagConstraints() ;
         constraints.gridx = column ;
         constraints.gridy = row ;
         constraints.fill = fill ;
         constraints.gridwidth = columns ;
         constraints.gridheight = rows ;
+        constraints.anchor = anchor ;
         return constraints ;
+    }
+    
+    /*
+     * Second most general setUpConstraints method
+     * Defaults:
+     *      anchor = CENTER -> centered in the space alloted
+     */
+    private static java.awt.GridBagConstraints setUpConstraints( int column , int row , 
+                                                                 int fill , int columns ,
+                                                                 int rows ) {
+        return setUpConstraints( column , row , fill , columns , rows , GBCENTER ) ;
     }
     
     /*
@@ -37,7 +61,7 @@ public class SquashIPRangeUINew {
      *      gridheight = 1 -> takes up one row
      */
     private static java.awt.GridBagConstraints setUpConstraints( int column , int row , int fill ) {
-        return setUpConstraints( column , row , fill , 1 , 1 ) ;
+        return setUpConstraints( column , row , fill , 1 , 1 , GBCENTER ) ;
     }
     /*
      * Least general setUpConstraints method
@@ -47,7 +71,7 @@ public class SquashIPRangeUINew {
      *      gridheight = 1 -> takes up one row
      */
     private static java.awt.GridBagConstraints setUpConstraints( int column , int row ) {
-        return setUpConstraints( column , row , GBNONE , 1 , 1 ) ;
+        return setUpConstraints( column , row , GBNONE , 1 , 1 , GBCENTER ) ;
     }
     
     public static void main( String[] args ) {
@@ -59,6 +83,26 @@ public class SquashIPRangeUINew {
         mainFrame.setLayout( new java.awt.GridBagLayout() ) ;
         
         /*
+         * The application will be split
+         * into three JPanels:
+         *   Input
+         *   Output
+         *   About/Exit
+         * Each will have a titled border
+         */
+        javax.swing.JPanel inputPanel = new javax.swing.JPanel( new java.awt.GridBagLayout() ) ;
+        inputPanel.setBorder( titledBorder( "Input" ) );
+        
+        javax.swing.JPanel outputPanel = new javax.swing.JPanel( new java.awt.GridBagLayout() ) ;
+        outputPanel.setBorder( titledBorder( "Output" ) ) ;
+        
+        javax.swing.JPanel miscPanel = new javax.swing.JPanel( new java.awt.GridBagLayout() ) ;
+        
+        mainFrame.add( inputPanel , setUpConstraints( 0 , 0 , GBBOTH ) ) ;
+        mainFrame.add( outputPanel , setUpConstraints( 1 , 0 , GBBOTH , 1 , 1 ) ) ;
+        mainFrame.add( miscPanel , setUpConstraints( 2 , 1 , GBBOTH , 1 , 1 , GBRIGHT ) ) ;
+        
+        /*
          * Buttons
          * Here we'll set up the buttons for the application
          * The basic layout for each will be
@@ -67,39 +111,50 @@ public class SquashIPRangeUINew {
          *      Binding function to perform on action
          */
         
-        //Reformat
+        //Reformat, in input panel
         javax.swing.JButton reformatButton = new javax.swing.JButton( "Reformat" ) ;
-        mainFrame.add( reformatButton , setUpConstraints( 1, 1, 
-                                                          java.awt.GridBagConstraints.BOTH ) ) ;
+        inputPanel.add( reformatButton , setUpConstraints( 1, 0, GBBOTH ) ) ;
         
-        //Squash
+        //Squash, in input panel
         javax.swing.JButton squashButton = new javax.swing.JButton( "Squash" ) ;
-        mainFrame.add( squashButton , setUpConstraints( 1, 2, GBBOTH ) ) ;
+        inputPanel.add( squashButton , setUpConstraints( 1, 1, GBBOTH ) ) ;
         
-        //Find Overlap
+        //Find Overlap, in input panel
         javax.swing.JButton overlapButton = new javax.swing.JButton( "Find Overlap" ) ;
-        mainFrame.add( overlapButton , setUpConstraints( 1, 3, GBBOTH ) ) ;
+        inputPanel.add( overlapButton , setUpConstraints( 1, 2, GBBOTH ) ) ;
         
-        //Clear Input
+        //Clear Input, in input panel
         javax.swing.JButton clearInputButton = new javax.swing.JButton( "Clear Input" ) ;
-        mainFrame.add( clearInputButton , setUpConstraints( 1, 4, GBBOTH ) ) ;
+        inputPanel.add( clearInputButton , setUpConstraints( 1, 3, GBBOTH ) ) ;
         
-        //To Clipboard
+        //To Clipboard, in output panel
         javax.swing.JButton clipboardButton = new javax.swing.JButton( "To Clipboard" ) ;
-        mainFrame.add( clipboardButton , setUpConstraints( 2, 1, GBBOTH ) ) ;
+        outputPanel.add( clipboardButton , setUpConstraints( 1, 0, GBBOTH ) ) ;
         
-        //Help
+        //Help, in misc panel
         javax.swing.JButton aboutButton = new javax.swing.JButton( "About" ) ;
-        mainFrame.add( aboutButton , setUpConstraints( 2, 4, GBBOTH)  ) ;
+        miscPanel.add( aboutButton , setUpConstraints( 0, 0, GBBOTH, 1, 1, GBRIGHT )  ) ;
          
        
-        //Close
+        //Close, in misc panel
         javax.swing.JButton closeButton = new javax.swing.JButton( "Close" ) ;
-        mainFrame.add( closeButton , setUpConstraints( 2, 5, GBBOTH ) ) ;
+        miscPanel.add( closeButton , setUpConstraints( 1, 0, GBBOTH, 1, 1, GBRIGHT ) ) ;
         
         /*
          * Input and output text areas
          */
+        
+        //Input, in input panel
+        javax.swing.JTextArea inputTextArea = new javax.swing.JTextArea( TEXTAREAHEIGHT, 
+                                                                         TEXTAREAWIDTH ) ;
+        inputPanel.add( inputTextArea, setUpConstraints( 0 , 0 , GBBOTH , 1 , 6 ) ) ;
+        inputTextArea.setLineWrap( true ) ;
+        
+        //Output, in output panel
+        javax.swing.JTextArea outputTextArea = new javax.swing.JTextArea( TEXTAREAHEIGHT, 
+                                                                          TEXTAREAWIDTH ) ;
+        outputPanel.add( outputTextArea, setUpConstraints( 0 , 0 , GBBOTH , 1 , 6 ) ) ;
+        outputTextArea.setLineWrap( true ) ;
         
         //Exit on close
         mainFrame.setDefaultCloseOperation( javax.swing.JFrame.EXIT_ON_CLOSE ) ;
